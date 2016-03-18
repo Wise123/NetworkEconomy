67fb6e65225d4aa955c0ef96fdf8d21b65cd1903
Wise <Wise@wise-pc>
2016-03-18 13:02:48 +0500

    2

--- EconomicServlet/src/controllers/PagesController.java
+++ EconomicServlet/src/controllers/PagesController.java
@@ -19,30 +19,25 @@ public class PagesController {
 	@RequestMapping("/index")
 	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse response) {
 		
-		 try
-	        {
-	            Class.forName("org.h2.Driver");
-	            Connection con = DriverManager.getConnection("jdbc:h2:/WEB-INF/h2", "test", "" );
-	            Statement stmt = con.createStatement();
-	            //stmt.executeUpdate( "DROP TABLE table1" );
-	            //stmt.execute( "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES" );
-	            //stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Claudio' )" );
-	            //stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Bernasconi' )" );
-	 
-	            ResultSet rs = stmt.executeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES");
-	            while( rs.next() )
-	            {
-	                String name = rs.getString("TABLE_NAME");
-	                System.out.println( name );
-	            }
-	            stmt.close();
-	            con.close();
-	        }
-	        catch( Exception e )
-	        {
-	            e.printStackTrace();
-	        }
-		
+		 try {
+			Class.forName("org.h2.Driver");
+			Connection con = DriverManager.getConnection("jdbc:h2:/WEB-INF/h2", "test", "" );
+			Statement stmt = con.createStatement();
+			//stmt.executeUpdate( "DROP TABLE table1" );
+			//stmt.execute( "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES" );
+			//stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Claudio' )" );
+			//stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Bernasconi' )" );
+			ResultSet rs = stmt.executeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES");
+			while( rs.next() ){
+				String name = rs.getString("TABLE_NAME");
+				System.out.println( name );
+			}
+			stmt.close();
+			con.close();
+		}
+		catch( Exception e ){
+			e.printStackTrace();
+		}
 		return new ModelAndView("/WEB-INF/jsp/index.jsp");
 	}
 }
