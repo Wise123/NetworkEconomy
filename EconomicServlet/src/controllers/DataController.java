8ef78feca1a39a841660448fda9314110e99a1fc
Kirill <Kirill@Kirill-PC.Dlink>
2016-04-13 19:47:43 +0500

    providers CRUD

--- EconomicServlet/src/controllers/DataController.java
+++ EconomicServlet/src/controllers/DataController.java
@@ -23,6 +23,7 @@ import dao.GoodsDao;
 import dao.ProvidersDao;
 import tables.Client;
 import tables.Good;
+import tables.Provider;
 import util.ApplicationContextProvider;
 
  
@@ -143,10 +144,16 @@ public class DataController {
 	}
 	//http://localhost:8080/EconomicServlet/deleteGood?idGood=4 does work
 	@RequestMapping("/deleteGood")
-	public void deleteGood(@RequestParam Integer idGood,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@ResponseBody
+	public String deleteGood(@RequestParam Integer idGood,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
 		GoodsDao goodsDao =  (GoodsDao) applicationContextProvider.getApplicationContext().getBean("GoodsDao");
 		goodsDao.deleteById(idGood);
-		request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+		}catch (Exception e){
+			return "{\"error:\":\""+e.getMessage()+"\"}";
+		}
+		//request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+		return "{\"status:\":\"OK\"}";
 	}
 	
 	
@@ -175,6 +182,55 @@ public class DataController {
 
 		//return new ModelAndView("/WEB-INF/jsp/login.jsp");
 	}
+	//http://localhost:8080/EconomicServlet/createProvider?jsonProvider={"idProvider":"5","address":"Tailand","title":"Law","description":"some descr"}
+	@RequestMapping(value = "/createProvider", method=RequestMethod.POST)
+	public void createProvider(HttpServletRequest request, HttpServletResponse response, @RequestParam(required=true) String jsonProvider) {
+		System.out.println(jsonProvider);
+		ProvidersDao providersDao =  (ProvidersDao) applicationContextProvider.getApplicationContext().getBean("ProvidersDao");
+		ObjectMapper mapper = new ObjectMapper();
+		Provider provider= null;
+		try {
+			provider = mapper.readValue(jsonProvider, Provider.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if (provider != null) {
+			providersDao.create(provider);
+		}
+		System.out.println(provider);
+	}
 	
+	//http://localhost:8080/EconomicServlet/updateProvider?jsonProvider={"idProvider":"5","address":"Tailand","title":"Law","description":"some updated descr"}
+	@RequestMapping(value = "/updateProvider", method=RequestMethod.POST)
+	public void updateProvider(HttpServletRequest request, HttpServletResponse response, @RequestParam(required=true) String jsonProvider) {
+		System.out.println(jsonProvider);
+		ProvidersDao providersDao =  (ProvidersDao) applicationContextProvider.getApplicationContext().getBean("ProvidersDao");
+		ObjectMapper mapper = new ObjectMapper();
+		Provider provider= null;
+		try {
+			provider = mapper.readValue(jsonProvider, Provider.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if (provider != null) {
+			providersDao.update(provider);
+		}
+		System.out.println(provider);
+	}
+	
+	@RequestMapping("/deleteProvider")
+	@ResponseBody
+	public String deleteProvider(@RequestParam Integer idProvider,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
+			ProvidersDao providersDao =  (ProvidersDao) applicationContextProvider.getApplicationContext().getBean("ProvidersDao");
+			providersDao.deleteById(idProvider);
+		}catch (Exception e){
+			return "{\"error:\":\""+e.getMessage()+"\"}";
+		}
+		//request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+		return "{\"status:\":\"OK\"}";
+	}
 	
 }
